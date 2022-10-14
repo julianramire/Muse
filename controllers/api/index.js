5708385c2ef8e5a80214b1cf68a22eef19d6c241
Nathan <nathan_ramirez19@yahoo.com>
2022-10-13 20:03:21 -0700

    changed file names

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+<<<<<<< HEAD
 const projectRoutes = require('./projectRoutes');
 const generate = require('./generate')
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 router.use('/generate', generate)
+=======
+const aiRoutes = require('./apiRoutes');
+
+router.use('/users', userRoutes);
+router.use('/ai', aiRoutes);
+>>>>>>> e67d737 (changed file names)
 
 module.exports = router;
