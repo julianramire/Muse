c75c6ecbaaa367578f8c649c9fc063c40b91bbcb
all-ram <ramirez.allan996@gmail.com>
2022-10-20 18:35:49 -0700

    final styles

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { AI, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // // Get all projects and JOIN with user data
     // const aiData = await AI.findAll({
